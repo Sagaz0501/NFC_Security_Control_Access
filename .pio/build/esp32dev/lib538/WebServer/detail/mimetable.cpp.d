ec7d491e9b2ab8a4e958ee1dc1700d9e624bf563
Sagaz0501 <sanbernale@gmail.com>
2025-11-12 15:56:36 -0500

    Limpia duplicados leds_control.* y mantiene led_control.*

--- .pio/build/esp32dev/lib538/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib538/WebServer/detail/mimetable.cpp.o: \
- /home/sagaz/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /home/sagaz/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /home/sagaz/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
